9bd2bdf01a05aa6e1e05c5bfc8d569c4b4dee54f
Matheus Penchel <matheus.penchel@gmail.com>
2020-09-11 09:27:55 -0300

    Added products to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Mastodon T-shirt", tagline: "shirt band cool")
+Product.create(name: "Pink Floyd T-shirt", tagline: "shirt band cool")
